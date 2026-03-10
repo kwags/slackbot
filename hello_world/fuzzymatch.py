98014d86c2a9d2d3eacb55a149f835ca37e04dd1
Kayla W <wagne285@uwm.edu>
2026-03-10 12:49:28 -0500

    Added logging info

--- hello_world/fuzzymatch.py
+++ hello_world/fuzzymatch.py
@@ -18,21 +18,24 @@ FAQ_QUESTIONS = [faq["question"] for faq in FAQ_LIST]
 # search faq for fuzzy match to user's message
 # if match ratio is 70%+
 # return corresponding answer, otherwise None
-def get_fuzzy_match(user_msg: str, threshold: int = 60) -> str | None:
+def get_fuzzy_match(user_msg: str, threshold: int = 75, return_score=False):
 
     # convert user message to lower case and remove white space
     user_message_lower = user_msg.lower().strip()
     
     result = process.extractOne(user_message_lower, FAQ_QUESTIONS, scorer=fuzz.token_set_ratio)
-   
+
+    if result is None:
+        return (None, 0, None) if return_score else None
+    
     best_match_question, score, index = result
 
-    if score >= threshold: 
-        return FAQ_LIST[index]["answer"]
-            
-    # if no fuzzy match, return none
-    if result is None:
-        return None
+    if score >= threshold:
+        answer = FAQ_LIST[index]["answer"]
+        if return_score:
+            return answer, score, best_match_question
+        return answer
+    return (None, 0, None) if return_score else None
 
 # -------------- #
 #      TEST
@@ -43,7 +46,8 @@ if __name__ == "__main__":
         "friends and family ticket link",
         "Where do I submit an LOA form?",
         "Leave of Absence",
-        "what is the email for the board?",
+        "what is the attendence policy?",
+        "what is the email address for the board?",
         "Do aliens really exist?"
     ]
 
